cd77753e45f852b8201a5a0b505f2d6be1fe24e9
Tshwanelo Goodwill Bucibo <bucibot@gmail.com>
2023-12-24 23:31:53 +0200

    initial commit

---
+++ database_naming_convention.js
@@ -0,0 +1,10 @@
+// https://dev.to/ovid/database-naming-standards-2061
+
+// Summary
+// Naming standards are important because it helps keep your code consistent and predictable. Anything violating that predictability tends to be suspect. However, well-chosen naming standards can make it easier to understand your code and, if done well, can often make it possible to see a error in a single line of code.
+
+// Use underscore_names instead of CamelCase
+// Table names should be plural eg: customers instead of customer
+// Spell out id fields (item_id instead of id)
+// Don't use ambiguous column names
+// When possible, name foreign key columns the same as the columns they refer to
---
+++ reactjs_naming_convention.js
@@ -0,0 +1,34 @@
+// https://www.linkedin.com/pulse/react-js-naming-convention-kristiyan-velkov/
+// https://climbtheladder.com/10-react-naming-conventions-best-practices/
+
+// Constants
+// Use uppercase letters with underscores to represent constants in JavaScript.
+// For example, API_URL, MAX_RESULTS.
+
+// CSS classes
+// Use lowercase letters and hyphens for CSS class names.
+
+// Components
+// Use descriptive and meaningful names for React components. Use PascalCase (capitalizing the first letter of each word) for component names.
+
+// Files
+// Name your files using PascalCase, matching the component name. For example, if you have a component named UserCard, the file should be named UserCard.js.
+
+// Props
+// Use descriptive names for props to clearly indicate their purpose. Avoid abbreviations or acronyms unless they are widely understood in the context of your project.
+// For example:
+// instead of usr, use user.
+
+// State variables
+// Prefix state variables with is, has, or should to denote boolean values.
+// example, we have three state variables:
+// isActive,
+// hasError
+// shouldRender
+// These variables are prefixed with is, has, and should, respectively, according to the naming convention
+
+// Event handlers
+// Use handle as a prefix for event handler functions. For example, handleClick, handleInputChange
+
+// Utility functions
+// For example, formatDate, generateUniqueId.
